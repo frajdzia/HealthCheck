b7d6eb6948f9978d0476bf935bb9b5e4737d61eb
frajdzia <72212007+frajdzia@users.noreply.github.com>
2025-03-25 09:36:43 +0000

    project set up created main app, global styling and base html file

---
+++ HealthCheck/urls.py
@@ -0,0 +1,16 @@
+"""
+URL configuration for HealthCheck project.
+
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
