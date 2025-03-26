6182dc8a5533ac35f0ac24ce043da9bd8db5fc7a
frajdzia <72212007+frajdzia@users.noreply.github.com>
2025-03-26 23:22:33 +0000

    styles update

--- HealthCheck/settings.py
+++ HealthCheck/settings.py
@@ -4,6 +4,7 @@ Django settings for HealthCheck project.
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,6 +102,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 
