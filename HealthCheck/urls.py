4d42c750e155ba9e6004f7d25544e2e6939f2d17
frajdzia <72212007+frajdzia@users.noreply.github.com>
2025-04-15 11:46:12 +0100

    Fix AppUser error, update home app (views, models, templates, CSS), clean pycache and untrack db.sqlite3

--- HealthCheck/urls.py
+++ HealthCheck/urls.py
@@ -2,16 +2,16 @@
 URL configuration for HealthCheck project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from home import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    #URL for admin page
+    path('admin/', admin.site.urls, name='admin'),
     
     # URL for home page
-    path('', views.home, name='home'),
+    path('', include('home.urls')),
 ]
 
 # Serve static files during development
--- home/models.py
+++ home/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
-# Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=255)
+    team_leader = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.team_name
+
+class Admin(models.Model):
+    user = models.ForeignKey('AppUser', on_delete=models.CASCADE, related_name='admin_roles')  # Added related_name
+    admin_id = models.CharField(max_length=255, primary_key=True)
+
+    def __str__(self):
+        return self.admin_id
+    
+class AppUser(models.Model):
+    django_user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
+    card = models.ForeignKey('Card', on_delete=models.CASCADE, null=True)
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE, null=True)
+    has_completed_form = models.BooleanField(default=False)  # New field
+
+    def __str__(self):
+        return self.django_user.username
+    
+class Card(models.Model):
+    votes = models.FloatField(null=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    trend = models.IntegerField()
+    state = models.IntegerField()
+    comments = models.TextField()
+
+    def __str__(self):
+        return f"Card {self.id}"
+
+class Session(models.Model):
+    date = models.DateTimeField(null=True)
+    update_progress = models.TextField()
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Session {self.id}"
