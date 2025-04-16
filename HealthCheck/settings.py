c322ddee9c97583aa287e3b523760232930400ea
Manjit997 <71028990+manjitks04@users.noreply.github.com>
2025-04-16 17:34:19 +0100

    fixed log in error redirect

--- HealthCheck/settings.py
+++ HealthCheck/settings.py
@@ -130,4 +130,7 @@ STATIC_ROOT = ""
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'dashboard'  # ensure that it will redirect to home.html after logging in
+# LOGIN_REDIRECT_URL = '/dashboard'  # ensure that it will redirect to home.html after logging in
+
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard/'
