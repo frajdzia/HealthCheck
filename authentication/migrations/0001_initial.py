c06f31aeff9888b1e799475df1bbd7ee53cd4055
traceytrinita <trinitatracey@gmail.com>
2025-04-17 21:17:24 +0100

    adding comments

--- authentication/migrations/0001_initial.py
+++ authentication/migrations/0001_initial.py
@@ -5,45 +5,52 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migration):
+class Migration(migrations.Migration): #defining the schema migrations for the database
 
-    initial = True
+    initial = True #the first migration in the authentication app
 
     dependencies = [
+        #migration depends on the user model
+        #user model is defined in the settings.AUTH_USER_MODEL
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
+        #contains all the database operations
         migrations.CreateModel(
-            name="Profile",
+            name="Profile", #name for the model created
             fields=[
+                #making the ID to be the primary key
                 (
                     "id",
                     models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
+                        auto_created=True,  #auto create and increment the ID
+                        primary_key=True,   #making the ID field to be the primary key
+                        serialize=False,    #to not include during serializing
+                        verbose_name="ID",  #make it to human-readable format
                     ),
                 ),
                 (
+                    #making a role for each user from the options that they select
                     "role",
                     models.CharField(
-                        choices=[
+                        choices=[   #the options of the roles for them to choose as a dropdown in forms
                             ("engineer", "Engineer"),
                             ("team-leader", "Team Leader"),
                             ("department-leader", "Department Leader"),
                             ("senior-manager", "Senior Manager"),
                             ("admin", "Admin"),
                         ],
-                        max_length=30,
+                        max_length=30,  #maximum length for the string
                     ),
                 ),
                 (
                     "user",
+                    #one-to-one relationship to link the profile to a user
+                    #each user can only have one profile, vise versa
                     models.OneToOneField(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        to=settings.AUTH_USER_MODEL,
+                        on_delete=django.db.models.deletion.CASCADE,    #to delete the profile is user is deleted
+                        to=settings.AUTH_USER_MODEL,    #link to user model which specified in settings
                     ),
                 ),
             ],
--- authentication/migrations/0002_alter_profile_role.py
+++ authentication/migrations/0002_alter_profile_role.py
@@ -5,23 +5,23 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
+    dependencies = [    #the migration depends on the authencatication app and 0001_initial.py, which means that changes can only be made once the 0001_initial.py runs
         ("authentication", "0001_initial"),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name="profile",
-            name="role",
+    operations = [  
+        migrations.AlterField(  #will make changes that was applied to the database in this migration
+            model_name="profile",   #where the change is happening
+            name="role",    #the field that can be modified
             field=models.CharField(
-                choices=[
+                choices=[    #options of the roles for them to choose as a dropdown in forms
                     ("engineer", "Engineer"),
                     ("team-leader", "Team Leader"),
                     ("department-leader", "Department Leader"),
                     ("senior-manager", "Senior Manager"),
                     ("admin", "Admin"),
                 ],
-                max_length=50,
+                max_length=50,      #max length for the role string
             ),
         ),
     ]
