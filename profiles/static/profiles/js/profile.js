2994efd0d86c1ddea79b6554c49a6165bc1a3b74
Manjit997 <71028990+manjitks04@users.noreply.github.com>
2025-04-18 22:05:58 +0100

    rendered styling + form logic

--- profiles/static/profiles/js/profile.js
+++ profiles/static/profiles/js/profile.js
@@ -1,25 +1,30 @@
+console.log("JS loaded!");
+
 document.addEventListener("DOMContentLoaded", function () {
     const editButton = document.getElementById("editButton");
     const saveButton = document.getElementById("saveButton");
-    const formFields = document.querySelectorAll("input, select");
+    const form = document.querySelector("form");
+    const formFields = form.querySelectorAll("input, select, textarea");
 
-    formFields.forEach((field) => {
-        if (field.id !== "role") {
-            field.disabled = true;
-        }
+    // save btn hidden here
+    saveButton.style.display = "none";
+
+    editButton.addEventListener("click", () => {
+        console.log(editButton);
+        formFields.forEach((field) => {
+            if (field.id !== "role") {
+                field.disabled = false; // all other fields apart from role not disabled
+            }
+        });
+
+        editButton.style.display = "none";
+        saveButton.style.display = "inline-block";
     });
 
-    if (editButton) {
-        editButton.addEventListener("click", () => {
-            formFields.forEach((field) => {
-                if (field.id !== "role") {
-                    field.disabled = false;
-                }
-            });
-
-            editButton.style.display = "none";
-            saveButton.disabled = false;
-            saveButton.style.display = "inline-block";
-        });
-    }
+    form.addEventListener("submit", (e) => {
+        const isFormEnabled = Array.from(formFields).some((field) => !field.disabled && field.id !== "role");
+        if (!isFormEnabled) {
+            e.preventDefault();
+        }
+    });
 });
