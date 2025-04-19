aaf00d9f587a0dde5fb6827c7b1192c4e7be6e64
Manjit997 <71028990+manjitks04@users.noreply.github.com>
2025-04-19 22:49:09 +0100

    edited profile due to security reasons

--- profiles/views.py
+++ profiles/views.py
@@ -8,10 +8,27 @@ def profile(request):
     if request.method == 'POST':
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Changes made successfully.")
-            return redirect('profile')
+            if form.has_changed():
+                changed_fields = form.changed_data
+                form.save()
+                
+                # checks whether user has changed username or password, if so then logs out user due to security reasons
+                if 'username' in changed_fields or 'password' in changed_fields:
+                    return redirect('login') 
+                else: 
+                    messages.success(request, "Changes made successfully.")
+                    return redirect('profile')
     else:
         form = UserUpdateForm(instance=request.user)
 
     return render(request, 'profiles/profile.html', {'form': form})
+
+def team_progress(request):
+    user = request.user
+    if user.is_authenticated:
+        if hasattr(user, 'profile') and user.profile.role == 'Department Leader':
+            return render(request, 'profiles/teamprogress_DL.html')
+        else:
+            return render(request, 'profiles/teamprogress_TM.html')
+    else:
+        return redirect('login')
