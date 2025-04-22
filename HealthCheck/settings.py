f97c501f22f72e6149e33c07ee5b3746f37e4d7f
Manjit997 <71028990+manjitks04@users.noreply.github.com>
2025-04-22 23:40:52 +0100

    included static files within static root

--- HealthCheck/settings.py
+++ HealthCheck/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = ""
+STATIC_ROOT = "staticfiles"
 
 
 # Default primary key field type
